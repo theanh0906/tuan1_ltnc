67bd53ad44eff64e279024f0c812b7e67b5705b3
theanh0906 <77972474+theanh0906@users.noreply.github.com>
2021-01-25 20:28:12 +0700

    Add files via upload

---
+++ bai1_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include<cstring>
+#include<string.h>
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b&&a==c)
+        cout<<"yes";
+    else cout<<"no";
+    return 0;
+}
